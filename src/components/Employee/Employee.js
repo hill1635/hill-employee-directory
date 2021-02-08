ba77546e1dbd6e7b14384859ee06f8e703f7abe7
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-02-08 16:16:18 -0700

    Formatting.

--- src/components/Employee/Employee.js
+++ src/components/Employee/Employee.js
@@ -1,27 +1,35 @@
 import React from "react";
 
 function Employee(props) {
-
   return (
     <div>
-      {props.employees.filter((employee) => {
-        if (props.search == "") {
-          return employee;
-        } else if (employee.name.first.toLowerCase().includes(props.search.toLowerCase()) || employee.name.last.toLowerCase().includes(props.search.toLowerCase())) {
-          return employee;
-        }
-}).map((employee) => (
-        <div>
-          <ul>
-            <li>
-              Name: {employee.name.first} {employee.name.last}
-            </li>
-            <li>Email: {employee.email}</li>
-            <li>Phone: {employee.cell}</li>
-            {/* <li>{employee.location}</li> */}
-          </ul>
-        </div>
-      ))}
+      {props.employees
+        .filter((employee) => {
+          if (props.search == "") {
+            return employee;
+          } else if (
+            employee.name.first
+              .toLowerCase()
+              .includes(props.search.toLowerCase()) ||
+            employee.name.last
+              .toLowerCase()
+              .includes(props.search.toLowerCase())
+          ) {
+            return employee;
+          }
+        })
+        .map((employee) => (
+          <div>
+            <ul>
+              <li>
+                Name: {employee.name.first} {employee.name.last}
+              </li>
+              <li>Email: {employee.email}</li>
+              <li>Phone: {employee.cell}</li>
+              {/* <li>{employee.location}</li> */}
+            </ul>
+          </div>
+        ))}
     </div>
   );
 }
